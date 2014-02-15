746ad9382a212018f56fb384b1c0938f37514f0f
Vaibhav Verma <vaibhav2614@gmail.com>
2014-02-14 21:58:32 -0500

    They call me Gulp compilation

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+
+gulp.task('default', function() {
+    gulp.src('scripts/src/main.js')
+        .pipe(browserify({
+            insertGlobals: true,
+            debug: true,
+        }))
+        .pipe(concat('bundle.js'))
+        .pipe(gulp.dest('scripts/compiled'))
+});
